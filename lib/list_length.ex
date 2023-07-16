0efc1ac5aef455c09289e0527789b1653323bb44
nunees <felipe_silva_nunes@hotmail.com>
2023-07-15 21:11:20 -0300

    Fix comments

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,7 +1,3 @@
-# Nesse desafio, você deverá criar uma função que, dada uma lista de números (exemplo `[1, 3, 6, 43, 6]`), calcule,
-# recursivamente, quantos elementos há na lista e retorne esse valor.
-# Para exemplo, temos a seguinte entrada e saída esperada pela função que recebe a lista:
-
 defmodule ListLength do
   def call(list), do: length(list, 0)
 
